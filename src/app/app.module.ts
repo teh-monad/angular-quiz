72780516f5c6a79e1c99f38495e47fbc89b03669
teh-monad <q472857@mvrht.net>
2019-03-25 13:21:33 +0200

    Update app.module.ts
    fix errors

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,7 @@ import { AppComponent } from './app.component';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
 
-import { FormsModule }   from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 import { HeroFormComponent } from './hero-form/hero-form.component';
 
 import { PowerPipe } from './power.pipe';
@@ -20,9 +20,9 @@ import { PopupComponent } from './popup/popup.component';
 import { PopupService } from './popup/popup.service';
 import { ClickMeComponent } from './click-me/click-me.component';
 
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HeroFormTemplateComponent } from './template/hero-form-template.component';
-import { HeroFormReactiveComponent } from './reactive/hero-form-reactive.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HeroFormTemplateComponent } from './hero-form-template/hero-form-template.component';
+import { HeroFormReactiveComponent } from './hero-form-reactive/hero-form-reactive.component';
 import { ForbiddenValidatorDirective } from './shared/forbidden-name.directive';
 import { IdentityRevealedValidatorDirective } from './shared/identity-revealed.directive';
 import { UniqueAlterEgoValidatorDirective } from './shared/alter-ego.directive';
